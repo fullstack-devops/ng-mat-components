9e5602b7a54e99548a7b2689decab20b97ac8b82
Eike Haller <58111764+eksrha@users.noreply.github.com>
2022-11-14 16:27:19 +0000

    fix export

--- projects/ng-mat-components/src/lib/fs-calendar/public-api.ts
+++ projects/ng-mat-components/src/lib/fs-calendar/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of ng-mat-components
  */
 
+export { FsCalendarPanelsComponent } from './calendar-panels/calendar-panels.component';
+export { FsCalendarTableComponent } from './calendar-table/fs-calendar-table.component';
 export * from './calendar.models';
+export { FsCalendarTableNameDirective } from './directives/fs-calendar-table-name.directive';
 export { FsCalendarModule } from './fs-calendar.module';
 export { FsCalendarService } from './services/fs-calendar.service';
