02ba1761ad014129911a4ac486d1b7fea753a37c
Eike Haller <58111764+eksrha@users.noreply.github.com>
2023-02-02 14:25:43 +0000

    fix missed imports

--- projects/ng-mat-components/src/fs-nav-frame/public-api.ts
+++ projects/ng-mat-components/src/fs-nav-frame/public-api.ts
@@ -3,10 +3,17 @@
  */
 
 export { FsNavFrameContentDirective } from './directives/fs-nav-frame-content.directive';
+export { FsNavFrameSidebarDirective } from './directives/fs-nav-frame-sidebar.directive';
 export { FsNavFrameComponent } from './fs-nav-frame.component';
 export { FsNavFrameModule } from './fs-nav-frame.module';
 export * from './fs-nav-frame.modules';
+export { FsNavUserProfileActionsDirective } from './fs-nav-user-profile/directives/fs-nav-user-profile-actions.directive';
+export { FsNavUserProfileNameDirective } from './fs-nav-user-profile/directives/fs-nav-user-profile-name.directive';
+export { FsNavUserProfileSubNameDirective } from './fs-nav-user-profile/directives/fs-nav-user-profile-subname.directive';
+export { FsNavUserProfileComponent } from './fs-nav-user-profile/fs-nav-user-profile.component';
 export { FsNavFrameToolbarCenterDirective } from './nav-frame-toolbar/directives/fs-nav-frame-toolbar-center.directive';
 export { FsNavFrameToolbarEndDirective } from './nav-frame-toolbar/directives/fs-nav-frame-toolbar-end.directive';
 export { FsNavFrameToolbarStartDirective } from './nav-frame-toolbar/directives/fs-nav-frame-toolbar-start.directive';
 export { FsNavFrameToolbarComponent } from './nav-frame-toolbar/fs-nav-frame-toolbar.component';
+
+
