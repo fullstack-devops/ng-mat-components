bc7724ba6061fe1eff88a0b0e5674c11a9445fca
eksrha <58111764+eksrha@users.noreply.github.com>
2023-02-13 09:49:01 +0100

    fix: word wrap in logo and profile-user

--- projects/lib-workspace/src/app/app.module.ts
+++ projects/lib-workspace/src/app/app.module.ts
@@ -8,8 +8,15 @@ import { de } from 'date-fns/locale';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { FsCalendarModule, FsCalendarService, FsMaterialFullModule, FsNavFrameModule } from 'projects/ng-mat-components/src/public-api';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FsCalendarModule, FsCalendarService, FsNavFrameModule } from 'projects/ng-mat-components/src/public-api';
 import { HomeComponent } from './content/home/home.component';
 import { ShowcaseCalendarPanelsComponent } from './content/showcase-calendar-panels/showcase-calendar-panels.component';
 import { ShowcaseCalendarTableComponent } from './content/showcase-calendar-table/showcase-calendar-table.component';
@@ -23,8 +30,15 @@ import { ShowcaseNavFrameComponent } from './content/showcase-nav-frame/showcase
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    /* Mat Modules */
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatSlideToggleModule,
     /* Lib modules */
-    FsMaterialFullModule,
     FsNavFrameModule,
     FsCalendarModule,
   ],
